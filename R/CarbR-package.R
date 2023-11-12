d3f8ce1b8aa34ea74d542e6218b47fa967387a62
Chinyere Ottah <ottahc@mcmaster.ca>
2023-11-12 14:47:20 -0500

    Initial commit

---
+++ R/CarbR-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' CarbR: A package that is helpful for creating surface features such as slope and aspect for surface analysis using DEM in R.
+#'
+#' This package provides a workflow of package creation using R stidio. The package includes a function
+#' to derive slope and aspect using DEM and also a documented for the function.
+#' The DEM used for the study site is also included in the package.
+#'
+#' @docType package
+#' @name CarbR-package
+#' @author Chinyere Ottah, School of Earth, Environment and Society, McMaster Univetsity
+#' @references \url{https://github.com/p/ChinyereRuth/CarbR/}
+NULL
